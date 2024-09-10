3c1b7f4999ec7ba19fc3fc9d4eee551aceea6c89
northmule <izm@zixn.ru>
2024-09-10 09:38:13 +0300

    fix запроса на обновление начислений по заказу

--- cmd/gophermart/main.go
+++ cmd/gophermart/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"github.com/northmule/gofermart/config"
 	"github.com/northmule/gofermart/db"
 	"github.com/northmule/gofermart/internal/accrual/api/client"
@@ -14,6 +15,7 @@ import (
 )
 
 func main() {
+	fmt.Println("Запуск приложения Gophermart")
 	if err := run(); err != nil {
 		log.Fatal(err)
 	}
@@ -24,11 +26,12 @@ func run() error {
 	if err != nil {
 		return err
 	}
+	logger.LogSugar.Info("Инициализация конфигурации")
 	cfg, err := config.NewGophermartConfig()
 	if err != nil {
 		return err
 	}
-
+	logger.LogSugar.Info("Инициализация базы данных")
 	store, err := storage.NewPostgresStorage(cfg.DatabaseURI)
 	if err != nil {
 		return err
@@ -38,16 +41,17 @@ func run() error {
 	if err != nil {
 		return err
 	}
-
+	logger.LogSugar.Info("Инициализация миграций")
 	migrations := db.NewMigrations(store.RawDB)
 	err = migrations.Up()
 	if err != nil {
 		return err
 	}
-
+	logger.LogSugar.Info("Инициализация менеджера репозитариев")
 	repositoryManager := repository.NewManager(store.DB)
 	routes := api.NewAppRoutes(repositoryManager)
 
+	logger.LogSugar.Info("Инициализация клиента Accrual и worker-ов")
 	accrualClient := client.NewAccrualClient(config.AccrualURLDefault, logger.LogSugar)
 	_ = job.NewWorker(repositoryManager, accrualClient)
 
