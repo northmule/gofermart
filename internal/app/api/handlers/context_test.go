fcad8394e43063fa90ea19997f4e2439573be16f
northmule <izm@zixn.ru>
2024-09-23 21:13:41 +0300

    тесты, мокки, сессии

--- internal/app/api/handlers/context_test.go
+++ internal/app/api/handlers/context_test.go
@@ -2,15 +2,16 @@ package handlers
 
 import (
 	"context"
+	"github.com/northmule/gophermart/internal/app/api/rctx"
 	"net/http"
 	"testing"
 )
 
 func TestAddCommonContext(t *testing.T) {
-	ctx := context.WithValue(context.Background(), "key", "value")
+	ctx := context.WithValue(context.Background(), rctx.UserCtxKey, "value")
 
 	nextHandler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if val, ok := r.Context().Value("key").(string); !ok || val != "value" {
+		if val, ok := r.Context().Value(rctx.UserCtxKey).(string); !ok || val != "value" {
 			t.Errorf("Expected context value 'value' but got %v", val)
 		}
 	})
